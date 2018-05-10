0686fbed8f6c6ec23ddcb74a2754da7645f015e8
VidyaTalla <vidhu.talla@gmail.com>
2018-05-09 17:24:27 -0700

    my second commit

---
+++ ApexTutorial/src/com/Apex/Assignment3/FindTriplet.java
@@ -0,0 +1,45 @@
+package com.Apex.Assignment3;
+
+public class FindTriplet {
+	 // returns true if there is triplet with sum equal
+    // to 'sum' present in A[]. Also, prints the triplet
+    boolean find3Numbers(int A[], int arr_size, int sum) 
+    {
+        int l, r;
+ 
+        // Fix the first element as A[i]
+        for (int i = 0; i < arr_size - 2; i++) 
+        {
+            // Fix the second element as A[j]
+            for (int j = i + 1; j < arr_size - 1; j++) 
+            {
+                // Now look for the third number
+                for (int k = j + 1; k < arr_size; k++) 
+                {
+                    if (A[i] + A[j] + A[k] == sum) 
+                    {
+                        System.out.print("Triplet is " + A[i] + " ," + A[j] 
+                                                              + " ," + A[k]);
+                        return true;
+                    }
+                }
+            }
+        }
+ 
+        // If we reach here, then no triplet was found
+        return false;
+    }
+ 
+    // Driver program to test above functions
+
+	public static void main(String[] args) {
+		  FindTriplet triplet = new FindTriplet();
+	        int A[] = {1,2,3,4,4,5,6,7,8,9};
+	        int sum = 10;
+	        int arr_size = A.length;
+	        triplet.find3Numbers(A, arr_size, sum);
+	    }
+		
+	}
+
+
---
+++ ApexTutorial/src/com/Apex/Assignment3/NonDuplicate.java
@@ -0,0 +1,28 @@
+package com.Apex.Assignment3;
+
+public class NonDuplicate {
+	static int firstNonDuplicateing(int arr[], int n)
+	{
+		for(int i=0; i<n; i++){
+			int j;
+			for(j=0;j<n;j++){
+				if(i != j && arr[i]==arr[j]){
+					break;
+				}
+				if(j==n){
+					return arr[i];
+				}
+			}
+		}
+		return n;
+	}
+
+	public static void main(String[] args) {
+		int arr[] = {9,4,9,6,7,4};
+		int n = arr.length;
+		System.out.println(firstNonDuplicateing(arr, n));
+
+		
+	}
+
+}
